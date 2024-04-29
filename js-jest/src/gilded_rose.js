6a283133231f5e69a54e1f93f5c2954511425ea5
Gavin Bones <gavin.bones@bt.com>
2024-04-29 21:57:51 +0000

    make conjured item test pass

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,6 +6,12 @@ class Item {
   }
 }
 
+class ConjuredItem extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+}
+
 const ItemNames = {
   AGED_BRIE : 'Aged Brie',
   BACKSTAGE_PASS : 'Backstage passes to a TAFKAL80ETC concert',
@@ -19,50 +25,55 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      
       const itemName = this.items[i].name;
       let itemQuality = this.items[i].quality;
       let itemSellIn = this.items[i].sellIn
 
-      if (itemName != ItemNames.AGED_BRIE && itemName != ItemNames.BACKSTAGE_PASS) {
-        if (itemQuality > 0) {
-          if (itemName != ItemNames.SULFURAS) {
-            itemQuality = itemQuality - 1;
-          }
-        }
+      if (this.items[i] instanceof ConjuredItem) {
+        itemQuality = itemQuality - 2;
       } else {
-        if (itemQuality < 50) {
-          itemQuality = itemQuality + 1;
-          if (itemName == ItemNames.BACKSTAGE_PASS) {
-            if (itemSellIn < 11) {
-              if (itemQuality < 50) {
-                itemQuality = itemQuality + 1;
-              }
+        if (itemName != ItemNames.AGED_BRIE && itemName != ItemNames.BACKSTAGE_PASS) {
+          if (itemQuality > 0) {
+            if (itemName != ItemNames.SULFURAS) {
+              itemQuality = itemQuality - 1;
             }
-            if (itemSellIn < 6) {
-              if (itemQuality < 50) {
-                itemQuality = itemQuality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (itemName != ItemNames.SULFURAS) {
-        itemSellIn = itemSellIn - 1;
-      }
-      if (itemSellIn < 0) {
-        if (itemName != ItemNames.AGED_BRIE) {
-          if (itemName != ItemNames.BACKSTAGE_PASS) {
-            if (itemQuality > 0) {
-              if (itemName != ItemNames.SULFURAS) {
-                itemQuality = itemQuality - 1;
-              }
-            }
-          } else {
-            itemQuality = itemQuality - itemQuality;
           }
         } else {
           if (itemQuality < 50) {
             itemQuality = itemQuality + 1;
+            if (itemName == ItemNames.BACKSTAGE_PASS) {
+              if (itemSellIn < 11) {
+                if (itemQuality < 50) {
+                  itemQuality = itemQuality + 1;
+                }
+              }
+              if (itemSellIn < 6) {
+                if (itemQuality < 50) {
+                  itemQuality = itemQuality + 1;
+                }
+              }
+            }
+          }
+        }
+        if (itemName != ItemNames.SULFURAS) {
+          itemSellIn = itemSellIn - 1;
+        }
+        if (itemSellIn < 0) {
+          if (itemName != ItemNames.AGED_BRIE) {
+            if (itemName != ItemNames.BACKSTAGE_PASS) {
+              if (itemQuality > 0) {
+                if (itemName != ItemNames.SULFURAS) {
+                  itemQuality = itemQuality - 1;
+                }
+              }
+            } else {
+              itemQuality = itemQuality - itemQuality;
+            }
+          } else {
+            if (itemQuality < 50) {
+              itemQuality = itemQuality + 1;
+            }
           }
         }
       }
@@ -76,6 +87,7 @@ class Shop {
 
 module.exports = {
   Item,
+  ConjuredItem,
   Shop,
   ItemNames
 }
